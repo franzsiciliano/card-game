aac189f46b287277bc2b21ee6398c77b5929c83b
Pierluigi <anacleriopierluigi@liber.it>
2014-04-08 16:32:14 +0200

    Carta da aggiungere modificata alle 16.32

--- src/it/skedenpio/cards/CardFrancesco.java
+++ src/it/skedenpio/cards/CardFrancesco.java
@@ -3,7 +3,7 @@ package it.skedenpio.cards;
 public class CardFrancesco extends Card {
 
 	public CardFrancesco() {
-		super("Carta di Fra", "Mè gioca mè...");
+		super("Carta di Fra", "Mè gioca mè...guadagni 10 punti e 2 posizioni...Che altro vuoi?");
 	}
 
 	@Override
